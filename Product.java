4aaa3e9262940771705cdef742809a17d0444160
Chris-Guffin <35351149+Chris-Guffin@users.noreply.github.com>
2018-11-28 15:46:21 -0800

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,37 @@
+package week8;
+
+import java.text.NumberFormat;
+
+public class Product {
+	
+	
+	private String code;
+	private String description;
+	private double price;
+	private static int count = 0;
+	
+	public Product(String code, String description, double price) {
+		this.code = code;
+		this.description = description;
+		this.price = price;
+		count++;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public String getPriceFormatted() {
+	    String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
+	    return formattedPrice;
+	    }
+	
+	public String toString() {
+		return("code: " + code + " description: " + description + " price: " + price);
+		
+	}
+}
