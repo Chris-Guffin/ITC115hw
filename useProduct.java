2cd441d77c69f106b7f6b2fd86355af1f536ec49
Chris-Guffin <35351149+Chris-Guffin@users.noreply.github.com>
2018-11-28 15:47:27 -0800

    Add files via upload

---
+++ useProduct.java
@@ -0,0 +1,17 @@
+package week8;
+
+//client program
+public class useProduct {
+
+	public static void main(String[] args) {
+		Product p1 = new Product("YYY", "Book", 14.99);
+		Product p2 = new Product("ZZZ", "Milk", 3.99);
+		Product p3 = new Product("WWW", "Wine", 9.99);
+
+		//print Product info
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+	}
+
+}
